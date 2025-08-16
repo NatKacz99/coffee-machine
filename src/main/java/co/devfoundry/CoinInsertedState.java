124d93aa4d6a1275bcc4ad73901b587aea99edb0
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-08-16 17:22:43 +0200

    add files

---
+++ src/main/java/co/devfoundry/CoinInsertedState.java
@@ -0,0 +1,25 @@
+package co.devfoundry;
+
+public class CoinInsertedState implements State{
+    @Override
+    public void insertTheCoin(CoffeeMachine coffeeMachine) {
+        System.out.println("Moneta została już wrzucona.");
+    }
+
+    @Override
+    public void pushTheButton(CoffeeMachine coffeeMachine) {
+        System.out.println("Nalewam kawę.");
+        coffeeMachine.state = new CupFullState();
+    }
+
+    @Override
+    public void takeTheCup(CoffeeMachine coffeeMachine) {
+        System.out.println("Wciśnij guzik.");
+    }
+
+    @Override
+    public void returnTheCoin(CoffeeMachine coffeeMachine) {
+        System.out.println("Zwracam monetę.");
+        coffeeMachine.state = new NoCoinState();
+    }
+}
